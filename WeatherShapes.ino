cd7cf0b4420e1946cb6437b9bb9fa7ae1056def2
Robert Burr <argorob70@gmail.com>
2015-10-04 16:54:28 -0500

    Still testing and getting to know GFX library methods.

--- WeatherShapes.ino
+++ WeatherShapes.ino
@@ -8,43 +8,29 @@
 
 // Create object for display screen
 Adafruit_HX8357 dScreen = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);
-  int screenWidth = dScreen.width();
+int screenWidth = dScreen.width();
+int screenHeight = dScreen.height();  
+int xScreen =0;
+int yScreen=0;
 void setup() {
-  dScreen.begin(HX8357D);
-  dScreen.setRotation(1);
-  dScreen.setCursor(0,0);
+dScreen.begin(HX8357D);
+dScreen.setRotation(1);
+dScreen.setCursor(0,0);
+dScreen.fillScreen(0);
 
 }
 
 
 void loop(void) {
-      dScreen.fillScreen(0);
-        dScreen.setCursor(0,0);
-        dScreen.println("Give me a name");
-        String readResult = Serial.readString();
-            delay(5000);
-      dScreen.setTextSize(5);
-    dScreen.print (readResult);
-    delay(5000);
-  
-  
-  /*for(uint8_t rotation=0; rotation<4; rotation++) {
-    dScreen.setRotation(rotation);
-    //testFillScreen();
-   // dScreen.fillScreen(0);
-    dScreen.println (screenWidth);
-    delay(1000);
-  }*/
+     dScreen.fillScreen(0);
+     dScreen.setTextSize(3);
+     dScreen.setCursor(xScreen,yScreen);
+     xScreen = xScreen + 5;
+     dScreen.print("Argorob");
+     delay(300);
+
 }
 
-unsigned long testFillScreen() {
-  unsigned long start = micros();
-  dScreen.fillScreen(HX8357_RED);
-  dScreen.fillScreen(HX8357_GREEN);
-  dScreen.fillScreen(HX8357_BLUE);
-  dScreen.fillScreen(HX8357_WHITE);
-  return micros() - start;
-}
 
 
 
